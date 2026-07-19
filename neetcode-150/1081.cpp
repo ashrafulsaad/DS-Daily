8e884f78d800a8de29475a85388c30ac52963baf
Ashrafulsaad <ashrafulsaad05@gmail.com>
2026-07-20 00:16:34 +0600

    leetcode daily 1081 problem has been solved

---
+++ neetcode-150/1081.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+
+        vector<int> freq(26,0);
+
+        for(char c:s)
+            freq[c-'a']++;
+
+        vector<bool> used(26,false);
+
+        stack<char> st;
+
+        for(char c:s){
+
+            freq[c-'a']--;
+
+            if(used[c-'a'])
+                continue;
+
+            while(!st.empty() &&
+                  st.top()>c &&
+                  freq[st.top()-'a']>0){
+
+                used[st.top()-'a']=false;
+                st.pop();
+            }
+
+            st.push(c);
+            used[c-'a']=true;
+        }
+
+        string ans="";
+
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(),ans.end());
+
+        return ans;
+    }
+};
